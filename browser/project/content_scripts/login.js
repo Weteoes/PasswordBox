6332b94a0b0a324b7bcf86a9f53467bcbd351ed6
Weteoes <mail@weteoes.cn>
2020-04-25 17:58:50 +0800

    添加google登录支持

--- browser/project/content_scripts/login.js
+++ browser/project/content_scripts/login.js
@@ -2,13 +2,12 @@ addDocumentEvent();
 // 保存全局变量
 let variable = {
     RunTimeConnect: null, // socket长连接
+    readyFunction: false, // 是否已经执行过
 }; 
 function log(...msg) {
     console.log(...msg);
 }
 function main() {
-    // 已经连接了一次就说明执行过了
-    if (variable.RunTimeConnect != null) { return; }
     let passInput = findPassInput();
     if (passInput == null) { 
         //log("找不到用户登录框 code:1"); 
@@ -21,24 +20,40 @@ function main() {
     }
     // 如果有登录框
     variable.input = { userInput: userInput, passInput: passInput };
-    // 连接background
-    socketConnect();
+    readyFunction();
     // 添加事件
     addInputEvent(userInput, passInput);
-    //log (userInput, passInput);
+}
+
+// 只会执行一次
+function readyFunction() {
+    if (variable.readyFunction) {
+        return;
+    }
+    log("Init");
+    variable.readyFunction = true;
+    // 连接background
+    socketConnect();
+    URL();
 }
 
 function socketConnect() {
-    variable.RunTimeConnect = chrome.runtime.connect();
-    variable.RunTimeConnect.onMessage.addListener(function(msg) {
+    try {
+        variable.RunTimeConnect = chrome.runtime.connect();
+        variable.RunTimeConnect.onMessage.addListener(function(msg) {
         switch(msg.api) {
             // 判断是否有保存账户的网站
             case "getUserPassbyUrl":
+                variable.input.userInput.focus()
                 variable.input.userInput.value = msg.user;
+                variable.input.passInput.focus()
                 variable.input.passInput.value = msg.pass;
                 break;
         }
-    });
+        });
+    } catch(e) {
+        log(e);
+    }
 }
 
 function addDocumentEvent() {
@@ -47,9 +62,11 @@ function addDocumentEvent() {
 
 function addInputEvent(userInput, passInput) {
     function PostMessageUser(user) {
+        if (variable.RunTimeConnect === null) { return; }
         variable.RunTimeConnect.postMessage( { api:"user", "user": user }, ()=> {})
     }
     function PostMessagePass(pass) {
+        if (variable.RunTimeConnect === null) { return; }
         variable.RunTimeConnect.postMessage( { api:"pass", "pass": pass }, ()=> {})
     }
     let user = userInput.value;
@@ -60,14 +77,14 @@ function addInputEvent(userInput, passInput) {
     if (pass != "") {
         PostMessagePass(pass);
     }
-    userInput.onchange = () => {
+    userInput.oninput = () => {
         let user = userInput.value;
-        if (user == "") { return; }
+        if (user === "") { return; }
         PostMessageUser(user);
     }
-    passInput.onchange = () => {
+    passInput.oninput = () => {
         let pass = passInput.value;
-        if (pass == "") { return; }
+        if (pass === "") { return; }
         PostMessagePass(pass);
     }
 }
@@ -86,16 +103,32 @@ function findPassInput() {
     return null;
 }
 
-function findUserInput(PassInput) {
-    let userInputType = "input[type=text]";
-    let a = document.querySelectorAll(userInputType);
-    if (a.length >= 1) {
+function findUserInput(passInput) {
+    const findByPassInput = () => {
+        let userInputType = "input[type=text]";
+        let a = document.querySelectorAll(userInputType);
+        if (a.length >= 1) {
+            return find(userInputType, passInput);
+        }
+        return null;
+    }
+
+    const findByType = () => {
+        let userInputType = "input[type=email]";
+        let a = document.querySelectorAll(userInputType);
+        if (a.length >= 1) {
+            return find(userInputType, passInput);
+        }
+        return null;
+    }
+
+    const find = (userInputType, passInput) => {
         // 如果有2个input以上，包括2个
         let passInputParent = null;
         while(1) {
             if (passInputParent == null) {
                 // 如果等于null，说明第一次寻找，等于passInput父级
-                passInputParent = PassInput.parentNode;
+                passInputParent = passInput.parentNode;
             } else {
                 // 不然就是多次，等于passInputParent父级
                 passInputParent = passInputParent.parentNode;
@@ -116,5 +149,19 @@ function findUserInput(PassInput) {
             }    
         }
     }
-    return null;
+    const result = findByType() || findByPassInput();
+    return result;
+}
+
+// 监听URL
+function URL() {
+    let lastURL = location.href;
+    function A() {
+        let nowURL = location.href;
+        if (lastURL == nowURL) { return; }
+        lastURL = nowURL;
+        log("页面已经切换");
+        main();
+    }
+    setInterval(A, 100);
 }
