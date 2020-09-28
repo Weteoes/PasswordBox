0ead332f5f05c62d90d4d7bf69984f0c02b9a851
Weteoes <mail@Weteoes.cn>
2020-09-28 21:14:55 +0800

    添加预编译，优化SEO

--- server/vue/vue.config.js
+++ server/vue/vue.config.js
@@ -1,3 +1,16 @@
-module.export = {
-  productionSourceMap: false
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
+module.exports = {
+  filenameHashing: false,
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, 'dist'),
+        routes: ['/']
+      })
+    ]
+  }
+  // options...
 }
